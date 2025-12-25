1dd9a6f5adac13d1bf3fdf6bb5c1e99f9dc5851a
shahils01 <shahils@clemson.edu>
2025-12-24 23:05:31 -0500

    exact fpo working for mujoco_playground. added train script

---
+++ playground/scripts/train_fpo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+env=FishSwim   
+seed=0
+
+echo "Exact FPO, env is ${env}, seed is ${seed}"
+python train_fpo.py \
+ --env-name ${env} \
+ --wandb-entity "shahil-shaik7-clemson-university" \
+ --wandb-project "FishSwim" \
+#  --config.gae-lambda 0.2 \
+#  --config.learning-rate 0.0001 \
+#  --config.discounting 0.98 \
+#  --config.num-envs 4096 \
+#  --config.clipping-epsilon 0.01 \
